907ff6cfa1aa41f07071919fd1fcb62d5d37d336
Frank Schmager <10031576+fschmager@users.noreply.github.com>
2023-10-12 19:44:52 +0000

    log

--- install.sh
+++ install.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 BASEDIR=$(dirname $(readlink -f "$0"))
-
-set -x
-
-sudo apt -y update
-
-sudo apt-get -y install \
-	htop 
-
-# install dotdrop and the workspace profile dotfiles
-pip3 install dotdrop
-dotdrop install -f -p workspace
+cd $BASEDIR
+scripts/setup-workspace.sh 2>&1 | tee ~/setup-workspace.log
+exit 0
