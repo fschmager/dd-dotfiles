05148d637c70fbd8b670f0018602180924b5ca4f
Frank Schmager <10031576+fschmager@users.noreply.github.com>
2023-10-13 13:56:16 +0000

    repos and stuff

--- scripts/setup-workspace.sh
+++ scripts/setup-workspace.sh
@@ -5,9 +5,22 @@ set -x
 
 sudo apt -y update
 
-sudo apt-get -y install \
+sudo apt -y upgrade
+
+sudo apt -y install \
 	htop 
+
+# install oh-my-bash
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
 
 # install dotdrop and the workspace profile dotfiles
 pip3 install dotdrop
 dotdrop install -f -p workspace
+
+# install repos
+setup-repo dogweb
+setup-repo dd-source
+setup-repo winter
+setup-repo team-aaa-internal-tools
+setup-repo k8s-resources
+setup-repo devtools
