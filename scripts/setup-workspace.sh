a9e13f5d5c89643d854391b8323b3a9f320691d1
Frank Schmager <10031576+fschmager@users.noreply.github.com>
2023-10-13 16:10:23 +0000

    unset url

--- scripts/setup-workspace.sh
+++ scripts/setup-workspace.sh
@@ -10,8 +10,11 @@ sudo apt -y upgrade
 sudo apt -y install \
 	htop 
 
+# unset to enable cloning because of missing ssh keys, dotdrop will override with new .gitconfig
+git config --global --remove-section url."git@github.com"
+
 # install oh-my-bash
-export OSH_REPOSITORY="git@github.com:ohmybash/oh-my-bash.git"; bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
 
 # install dotdrop and the workspace profile dotfiles
 pip3 install dotdrop --user
