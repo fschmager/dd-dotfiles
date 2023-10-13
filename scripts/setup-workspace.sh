174dab540378133a5f51fce4d15ffec42e2fdbff
Frank Schmager <10031576+fschmager@users.noreply.github.com>
2023-10-13 15:11:29 +0000

    git unset

--- scripts/setup-workspace.sh
+++ scripts/setup-workspace.sh
@@ -10,17 +10,12 @@ sudo apt -y upgrade
 sudo apt -y install \
 	htop 
 
+# avoid git vs https scheme issues
+git config unset
+
 # install oh-my-bash
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
 
 # install dotdrop and the workspace profile dotfiles
-pip3 install dotdrop
+pip3 install dotdrop --user
 dotdrop install -f -p workspace
-
-# install repos
-setup-repo dogweb
-setup-repo dd-source
-setup-repo winter
-setup-repo team-aaa-internal-tools
-setup-repo k8s-resources
-setup-repo devtools
