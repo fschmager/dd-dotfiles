ce44032065c08325f028e3ef32e0e3700708549d
Frank Schmager <10031576+fschmager@users.noreply.github.com>
2023-10-13 16:18:49 +0000

    correct section?

--- scripts/setup-workspace.sh
+++ scripts/setup-workspace.sh
@@ -11,7 +11,7 @@ sudo apt -y install \
 	htop 
 
 # unset to enable cloning because of missing ssh keys, dotdrop will override with new .gitconfig
-git config --global --remove-section url."git@github.com"
+git config --global --remove-section "url.git@github.com:"
 
 # install oh-my-bash
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
