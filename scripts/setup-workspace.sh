d77a8af23c8829de0c724efcccc9a3e37b794873
Frank Schmager <10031576+fschmager@users.noreply.github.com>
2023-10-20 16:08:47 +0000

    freephite

--- scripts/setup-workspace.sh
+++ scripts/setup-workspace.sh
@@ -3,12 +3,15 @@ BASEDIR=$(dirname $(readlink -f "$0"))
 
 set -x
 
+# pull package updates
 sudo apt -y update
 
+# update installed packages
 sudo apt -y upgrade
 
+# install new packages
 sudo apt -y install \
-	htop 
+	htop
 
 # unset to enable cloning because of missing ssh keys, dotdrop will override with new .gitconfig
 git config --global --remove-section "url.git@github.com:"
@@ -19,3 +22,14 @@ bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/mast
 # install dotdrop and the workspace profile dotfiles
 pip3 install dotdrop --user
 dotdrop install -f -p workspace
+
+# install bazel tools
+go install github.com/bazelbuild/buildtools/buildifier@latest
+go install mvdan.cc/gofumpt@latest
+
+# install freephite
+sudo chmod -R g+w /usr/local/volta
+volta install @bradymadden97/freephite-cli
+
+# install grpcurl
+go install github.com/fullstorydev/grpcurl/cmd/grpcurl@latest
